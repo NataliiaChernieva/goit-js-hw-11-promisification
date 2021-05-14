62204f9a7bb60e534f5f112a35d93ca21f0cf07c
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-05-14 18:49:11 +0300

    готовое решение 3

--- promisification.js
+++ promisification.js
@@ -59,21 +59,21 @@ const randomIntegerFromInterval = (min, max) => {
 const makeTransaction = (transaction) => {
   const delay = randomIntegerFromInterval(200, 500);
 
-  return new Promise((resolve, reserve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        onSuccess(transaction.id, delay);
+        resolve([transaction.id, delay]);
       } else {
-        onError(transaction.id);
+        reject(transaction.id);
       }
     }, delay);
   });
 };
 
-const logSuccess = (id, time) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
+const logSuccess = (arr) => {
+  console.log(`Transaction ${arr[0]} processed in ${arr[1]}ms`);
 };
 
 const logError = id => {
@@ -94,14 +94,14 @@ makeTransaction({ id: 70, amount: 150 })
   .then(logSuccess)
   .catch(logError);
 
-makeTransaction({ id: 71, amount: 230 })
-  .then(logSuccess)
-  .catch(logError);
+// makeTransaction({ id: 71, amount: 230 })
+//   .then(logSuccess)
+//   .catch(logError);
 
-makeTransaction({ id: 72, amount: 75 })
-  .then(logSuccess)
-  .catch(logError);
+// makeTransaction({ id: 72, amount: 75 })
+//   .then(logSuccess)
+//   .catch(logError);
 
-makeTransaction({ id: 73, amount: 100 })
-  .then(logSuccess)
-  .catch(logError);
+// makeTransaction({ id: 73, amount: 100 })
+//   .then(logSuccess)
+//   .catch(logError);
